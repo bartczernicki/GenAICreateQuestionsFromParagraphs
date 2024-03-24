bf1577f6e393ae2b0c14dc591057dbae90866e93
Bart Czernicki <bartczernicki@outlook.com>
2024-03-24 17:36:49 -0400

    Added processing options

---
+++ GenAICreateQuestionsFromParagraphs/ProcessingOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenAICreateQuestionsFromParagraphs
+{
+    enum ProcessingOptions
+    {
+        None = 0,
+        CreateQuestions = 1,
+        AnswerQuestions = 2,
+        AnswerQuestionsAtScale = 3
+    }
+}
